5295b320d69015c4c0aea0b0118189f373f108e2
RamazanUstuntas <ustuntasramazan@outlook.com>
2021-12-26 14:09:14 +0300

    Edited Template and Create Template Engine

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARS
+app.use(express.static('public'));
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
