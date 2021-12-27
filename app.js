410d6f5fca3f410dc05fdad565b8b224606003ad
RamazanUstuntas <ustuntasramazan@outlook.com>
2021-12-27 20:48:01 +0300

    added singular photo detail page

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+
+
 
 const app = express();
+
+//CONNECT DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
-//MIDDLEWARS
+//MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
+});
+app.get('/photos/:id', async (req, res) => {
+  //res.render('about');
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo', {
+    photo
+  })
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -20,7 +42,13 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
-
+app.get('/photo', (req,res) => {
+  res.render('photo');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
