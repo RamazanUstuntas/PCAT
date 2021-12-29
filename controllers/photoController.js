79e5e7c8940f12932eaab357f5a1c9c6aec30ef1
RamazanUstuntas <ustuntasramazan@outlook.com>
2021-12-29 18:57:18 +0300

    pagination created

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,11 +1,20 @@
 const Photo = require('../models/Photo');
 const fs = require('fs');
 const path = require('path');
+const { createSecureContext } = require('tls');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
+  const page = req.query.page || 1;
+  const photosPerPage = 3;
+  const totalPhotos = await Photo.find().countDocuments();
+  const photos = await Photo.find()
+    .sort('-dateCreated')
+    .skip((page - 1) * photosPerPage)
+    .limit(photosPerPage);
   res.render('index', {
-    photos,
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage),
   });
 };
 
@@ -17,37 +26,36 @@ exports.getPhoto = async (req, res) => {
 };
 
 exports.uploadPhoto = async (req, res) => {
-   const uploadDir = 'public/uploads';
-   if (!fs.existsSync(uploadDir)) {
-     fs.mkdirSync(uploadDir);
-   }
- 
-   let uploadedImage = req.files.image;
-   let uploadPath = __dirname + '/../public/uploads/' + uploadedImage.name;
- 
-   uploadedImage.mv(uploadPath, async () => {
-     await Photo.create({
-       ...req.body,
-       image: '/uploads/' + uploadedImage.name,
-     });
-     res.redirect('/');
-   });
- };
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
 
- exports.updatePhoto = async (req, res) => {
-   const photo = await Photo.findOne({ _id: req.params.id });
-   photo.title = req.body.title;
-   photo.description = req.body.description;
-   photo.save();
- 
-   res.redirect(`/photos/${req.params.id}`);
- };
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/../public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
+};
+
+exports.updatePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
+};
 
 exports.deletePhoto = async (req, res) => {
-   const photo = await Photo.findOne({_id: req.params.id});
-   let deletedImage = __dirname + '/../public' + photo.image;
-   fs.unlinkSync(deletedImage);
-   await Photo.findByIdAndDelete(req.params.id);
-   res.redirect('/');
- };
- 
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/../public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndDelete(req.params.id);
+  res.redirect('/');
+};
